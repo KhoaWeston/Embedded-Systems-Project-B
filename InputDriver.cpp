4f8165e2a9a103c7cd3c5d115e87a01437d0efda
Kyle Martin <bigguynh2003@gmail.com>
2024-10-08 14:19:30 -0400

    Update InputDriver.cpp

--- InputDriver.cpp
+++ InputDriver.cpp
@@ -23,6 +23,56 @@ InputDriver::Assign(gpio,gpio){
 
 }
 
-InputDriver::stateMachine(gpio){
-
+InputDriver::stateMachine(GPIO_TypeDef* gpio1, GPIO_TypeDef* gpio2){
+	static uint8_t state = 0;
+	bool printFlag;
+	bool Astate = HAL_GPIO_ReadPin(GPIOA, GPIO_Pin_6);
+	bool Bstate = HAL_GPIO_ReadPin(GPIOA, GPIO_Pin_7);
+	switch (state) {
+		default:
+			state = 0;
+			break;
+		case 0:                         // Idle state, encoder not turning
+			if (!Astate){             // Turn clockwise and A goes low first
+				state = 1;
+			} else if (!Bstate) {      // Turn anticlockwise and B goes low first
+				state = 4;
+			}
+			break;
+		// Clockwise rotation
+		case 1:
+			if (!Bstate) {             // Continue clockwise and B will go low after A
+				state = 2;
+			}
+			break;
+		case 2:
+			if (Astate) {             // Turn further and A will go high first
+				state = 3;
+			}
+			break;
+		case 3:
+			if (Astate && Bstate) {  // Both A and B now high as the encoder completes one step clockwise
+				state = 0;
+				printFlag = true; //increment after clockwise turn
+			}
+			break;
+		// Anticlockwise rotation
+		case 4:                         // As for clockwise but with A and B reversed
+			if (!Astate) {
+				state = 5;
+			}
+			break;
+		case 5:
+			if (Bstate) {
+				state = 6;
+			}
+			break;
+		case 6:
+			if (Astate && Bstate) {
+				state = 0;
+				printFlag = true; //decrement after anticlockwise turn
+			}
+			break;
+	}
+	return printFlag;
 }
