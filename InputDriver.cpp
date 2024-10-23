66f371a99c1a37d929d0c89fa87edbfed1dd7658
Kyle Martin <bigguynh2003@gmail.com>
2024-10-23 00:59:55 -0400

    Button Class and Input Driver Edits

--- InputDriver.cpp
+++ InputDriver.cpp
@@ -1,78 +1,66 @@
-/*
- * InputDriver.cpp
- *
- *  Created on: Oct 1, 2024
- *      Author: kcmartin
- */
-
-#include "InputDriver.h"
-
-InputDriver::InputDriver(Queue*){
-
-}
-
-InputDriver::getADD(){
-	return ADD;
-}
-
-InputDriver::setup(){
-
-}
-
-InputDriver::Assign(gpio,gpio){
-
-}
-
-InputDriver::stateMachine(GPIO_TypeDef* gpio1, GPIO_TypeDef* gpio2){
-	static uint8_t state = 0;
-	bool printFlag;
-	bool Astate = HAL_GPIO_ReadPin(GPIOA, GPIO_Pin_6);
-	bool Bstate = HAL_GPIO_ReadPin(GPIOA, GPIO_Pin_7);
-	switch (state) {
-		default:
-			state = 0;
-			break;
-		case 0:                         // Idle state, encoder not turning
-			if (!Astate){             // Turn clockwise and A goes low first
-				state = 1;
-			} else if (!Bstate) {      // Turn anticlockwise and B goes low first
-				state = 4;
-			}
-			break;
-		// Clockwise rotation
-		case 1:
-			if (!Bstate) {             // Continue clockwise and B will go low after A
-				state = 2;
-			}
-			break;
-		case 2:
-			if (Astate) {             // Turn further and A will go high first
-				state = 3;
-			}
-			break;
-		case 3:
-			if (Astate && Bstate) {  // Both A and B now high as the encoder completes one step clockwise
-				state = 0;
-				printFlag = true; //increment after clockwise turn
-			}
-			break;
-		// Anticlockwise rotation
-		case 4:                         // As for clockwise but with A and B reversed
-			if (!Astate) {
-				state = 5;
-			}
-			break;
-		case 5:
-			if (Bstate) {
-				state = 6;
-			}
-			break;
-		case 6:
-			if (Astate && Bstate) {
-				state = 0;
-				printFlag = true; //decrement after anticlockwise turn
-			}
-			break;
-	}
-	return printFlag;
-}
+///*
+// * InputDriver.cpp
+// *
+// *  Created on: Oct 1, 2024
+// *      Author: kcmartin
+// */
+//
+//#include "InputDriver.h"
+//
+//#define BTN_PRESSED GPIO_PIN_RESET //GPIO_PIN_RESET = 0
+//#define BTN_DEPRESSED GPIO_PIN_SET //GPIO_PIN_SET = 1
+//
+//
+//
+//InputDriver::InputDriver(Queue* line){
+//	queue = line;
+//	next=OFF;
+//	btnLastState=BTN_DEPRESSED;
+//}
+//
+//
+//InputDriver::btnAssign(Button btn){
+//}
+//
+//InputDriver::btnStateMachine(Button btn){
+//	static int state = 0;
+//	switch(state){
+//	case 0:
+//		//button is idle and not being pressed
+//		if (btn.state()==0 && btn.state()!=btnLastState){//button is pressed check for bounce
+//			btnLastState=btn.state();
+//			state=1;
+//			return false;
+//		}
+//		else{ //button is still idle
+//			state=0;
+//			return false;
+//		}
+//		break;
+//	case 1:
+//		//button pressed down, check for bounce
+//		if (btn.state()==btnLastState){//button was in fact pressed down
+//			btnLastState=btn.state();
+//			state=2;
+//			return true;
+//		}
+//		else{ //button was not pressed down
+//			btnLastState=btn.state();
+//			state=0;
+//			return false;
+//		}
+//		break;
+//	case 2:
+//		//button is pressed down, waiting for release
+//		if (btn.state()==1 && btn.state()!=btnLastState){//button was released, check for bounce
+//			btnLastState=btn.state();
+//			state=0;
+//			return false;
+//		}
+//		else{ // button was not released
+//			btnLastState=btn.state();
+//			state=1;
+//			return false;
+//		}
+//	}
+//}
